b7ef01eb1b43503fb48c1ceb56a8466107e7f4d8
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-24 15:16:09 +0400

    feat: get names

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import express from "express";
-
+import * as cheerio from "cheerio";
+import axios from "axios";
 const app = express();
 const port = process.env.PORT || 5001;
+const url = "https://www.bdh-online.de/patienten/therapeutensuche/?seite=1";
 let results = [];
+
+const fetchData = async () => {
+  try {
+    let response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const results = [];
+    $("tbody > tr").each((i, el) => {
+      const name = $(el).find("td:nth-child(1)").text().trim();
+
+      const person = {
+        name: name,
+      };
+      results.push(person);
+    });
+
+    console.log(results);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+fetchData();
 
 app.get("/person", (req, res) => {
   res.send(results);
